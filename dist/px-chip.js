3357326f3a842290a125dc125fa98a9287d032a8
Tali Marcus <tali.marcus@ge.com>
2017-09-14 17:36:41 -0700

    re-add dist folder and move js code into es6.js file

---
+++ dist/px-chip.js
@@ -0,0 +1,30 @@
+'use strict';(function(){Polymer({is:'px-chip',properties:{/**
+      * The text to display in the chip
+      *
+      * @property content
+      */content:{type:String},/**
+      * Set to true to show an icon in the chip, i.e. to create an Actionable Chip.
+      * Set the `isSelectable` property to false when using an Actionable Chip.
+      *
+      * @property selected
+      */showIcon:{type:Boolean,value:false},/**
+      * An svg icon from px-icon-set that will be displayed to the right of the
+      * text content (if `showIcon` is set to true).
+      * If nothing is specified for `icon`, the icon will default to an X (px-utl:close).
+      *
+      * @property icon
+      */icon:{type:String,value:'px-utl:close'},/**
+      * Set to true to select this chip. This property can be used with a Static Chip.
+      *
+      * @property selected
+      */selected:{type:Boolean,notify:true,value:false},/**
+      * Set this property to true when `showIcon` is true. The chip should not
+      * be selectable when an icon is showing (i.e. if it is an Actionable Chip).
+      *
+      * @property notSelectable
+      */notSelectable:{type:Boolean,value:false},/**
+      * A reference to this HTML element. Used by iron-a11y-keys.
+      *
+      * @property _el
+      */_el:{type:HTMLElement}},listeners:{'tap':'_handleActivated'},attached:function attached(){this.setAttribute('tabindex','0');this._el=this},_handleActivated:function _handleActivated(e){e.stopPropagation();if(!this.notSelectable){this.selected=!this.selected}this.fire('px-chip-activated')}})})();
+//# sourceMappingURL=px-chip.js.map
