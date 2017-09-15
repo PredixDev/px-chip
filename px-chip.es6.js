8f6cfea64f84a721a5b31d289ca0bfc384639854
Tali Marcus <tali.marcus@ge.com>
2017-09-14 17:35:03 -0700

    add accessibility; update layout to work without fit-content

--- px-chip.es6.js
+++ px-chip.es6.js
@@ -1,37 +1,80 @@
 (function() {
   Polymer({
 
-    is: 'px-chip', 
+    is: 'px-chip',
 
     properties: {
       /**
-      * This property keeps track of the number of clicks.
+      * The text to display in the chip
       *
-      * @property counterValue
+      * @property content
       */
-      counterValue: {
-        type: Number,
-        value: 0,
-        notify: true
+      content: {
+        type: String
+      },
+      /**
+      * Set to true to show an icon in the chip, i.e. to create an Actionable Chip.
+      * Set the `isSelectable` property to false when using an Actionable Chip.
+      *
+      * @property selected
+      */
+      showIcon: {
+        type: Boolean,
+        value: false
+      },
+      /**
+      * An svg icon from px-icon-set that will be displayed to the right of the
+      * text content (if `showIcon` is set to true).
+      * If nothing is specified for `icon`, the icon will default to an X (px-utl:close).
+      *
+      * @property icon
+      */
+      icon: {
+        type: String,
+        value: 'px-utl:close'
+      },
+      /**
+      * Set to true to select this chip. This property can be used with a Static Chip.
+      *
+      * @property selected
+      */
+      selected: {
+        type: Boolean,
+        notify: true,
+        value: false
+      },
+      /**
+      * Set this property to true when `showIcon` is true. The chip should not
+      * be selectable when an icon is showing (i.e. if it is an Actionable Chip).
+      *
+      * @property notSelectable
+      */
+      notSelectable: {
+        type: Boolean,
+        value: false
+      },
+      /**
+      * A reference to this HTML element. Used by iron-a11y-keys.
+      *
+      * @property _el
+      */
+      _el: {
+        type: HTMLElement
       }
     },
-
-    /**
-    * Handles click on the element defined in 'on-click' on the template.
-    *
-    * @method handleClick
-    */
-    handleClick: function(event, detail, sender) {
-      this.increment();
+    listeners: {
+      'tap': '_handleActivated'
     },
-
-    /**
-    * Increments the counter
-    *
-    * @method increment
-    */
-    increment: function() {
-      this.counterValue++;
+    attached() {
+      this.setAttribute("tabindex", "0");
+      this._el = this;
+    },
+    _handleActivated(e) {
+      e.stopPropagation();
+      if (!this.notSelectable) {
+        this.selected = !this.selected;
+      }
+      this.fire('px-chip-activated');
     }
   });
 })();
